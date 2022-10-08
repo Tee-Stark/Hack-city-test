48a13b0bce337d397fdc44d38b8a913412f265b8
Timmy Omolana <timmieomolanakiel@gmail.com>
2022-10-08 13:50:29 +0100

    Test four solution

---
+++ testFour.js
@@ -0,0 +1,18 @@
+// TEST FOUR
+// Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+// Return the running sum of nums
+
+const runningSum = (nums) => {
+  let sum = 0;
+  let answer = [];
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+    answer.push(sum);
+  }
+
+  return answer;
+};
+
+console.log(runningSum([1, 2, 3, 4]));
+console.log(runningSum([3, 1, 2, 10, 1]));
+console.log(runningSum([1, 1, 1, 1, 1]));
